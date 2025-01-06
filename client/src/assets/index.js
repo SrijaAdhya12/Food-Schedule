41d488a162387f4a4dbef5b3343a3e6a54bf1781
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2025-01-06 22:59:07 +0530

    feat: added assets

---
+++ client/src/assets/index.js
@@ -0,0 +1,6 @@
+export { default as dosa } from "./dosa.jpg";
+export { default as suji } from "./suji.jpg";
+export { default as rice } from "./rice.jpg";
+export { default as samosa } from "./samosa.jpg";
+export { default as ruti } from "./ruti.jpg";
+export { default as pakora } from "./pakora.jpg";
