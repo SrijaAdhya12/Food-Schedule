0ae0e5667159d2b3deb92a4e2de302baea302dc4
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2025-01-12 09:43:46 +0530

    feat: added current day as default

--- client/src/components/meal-planner.jsx
+++ client/src/components/meal-planner.jsx
@@ -7,7 +7,14 @@ import { ModeToggle } from './mode-toggle'
 
 
 const MealPlanner = () => {
-    const [selectedDay, setSelectedDay] = useState('Monday')
+
+    const getCurrentDay = () => {
+        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        const today = new Date().getDay();
+        return days[today];
+    }
+
+    const [selectedDay, setSelectedDay] = useState(getCurrentDay())
 
     const handleDayChange = (value) => {
         setSelectedDay(value)
